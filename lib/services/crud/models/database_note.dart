bf86465f09204f38ddc69ade429157fd417802dd
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-05-28 21:51:03 +0100

    moved these models to a models folder for proper structuring

---
+++ lib/services/crud/models/database_note.dart
@@ -0,0 +1,38 @@
+
+import '../crud_constants.dart';
+
+class DatabaseNote {
+  final int id;
+  final int userId;
+  final String text;
+  final bool isSyncedWithCloud;
+
+  DatabaseNote({
+    required this.id,
+    required this.userId,
+    required this.text,
+    required this.isSyncedWithCloud,
+  });
+
+  DatabaseNote.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        userId = map[userIdColumn] as int,
+        text = map[textColumn] as String,
+        isSyncedWithCloud =
+            (map[isSyncedWithCloudColumn] as int) == 1 ? true : false;
+
+  @override
+  String toString() {
+    return 'DatabaseNote: ID = $id, userId = $userId, isSyncedWithCloud = $isSyncedWithCloud,text = $text, )';
+  }
+
+  @override
+  bool operator ==(covariant DatabaseNote other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id;
+  }
+
+  @override
+  int get hashCode => id.hashCode;
+}
---
+++ lib/services/crud/models/database_user.dart
@@ -0,0 +1,25 @@
+
+import '../crud_constants.dart';
+
+class DatabaseUser {
+  final int id;
+  final String email;
+
+  const DatabaseUser({
+    required this.id,
+    required this.email,
+  });
+
+  DatabaseUser.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+
+  @override
+  String toString() => 'Person: ID = $id, email = $email';
+
+  @override
+  bool operator ==(covariant DatabaseUser other) => other.id == id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
